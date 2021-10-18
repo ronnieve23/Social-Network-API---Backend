023ca7b8d8a9f7652ffe71ce504234e76218eb0b
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-10-18 16:01:36 -0400

    added populate to user controller to display thoughts

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,6 +4,11 @@ const userController = {
     //get all users
     getAllUsers (req, res){
         User.find({})
+        .populate({
+            path:'thoughts',
+            select:'-__v'
+        })
+        .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch (err => {
             console.log(err);
